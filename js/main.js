0763932ee312dad63d97f7bc7ea7182591774d06
Mustafa Ramazanov <mustafa.ramazanov.2002@gmail.com>
2023-07-07 00:59:31 +0300

    repari burger-menu

--- js/main.js
+++ js/main.js
@@ -29,11 +29,14 @@
 
 });
 
+
+
 const menuBtn = document.querySelector('.menu__btn');
 const menu = document.querySelector('.menu');
 
 menuBtn.addEventListener('click', function(){
     menu.classList.toggle('menu--active');
+        menuBtn.classList.toggle('active')
 })
 
 
