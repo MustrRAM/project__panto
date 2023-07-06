0a7900a7e61faa82b6c90494141022b0b25f84e3
Mustafa Ramazanov <mustafa.ramazanov.2002@gmail.com>
2023-07-07 02:09:58 +0300

    experiment

--- js/main.js
+++ js/main.js
@@ -66,3 +66,21 @@ function clearActiveClasses(){
         price.classList.remove('price__bar-list--active')
     })
 }
+
+
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('#animation');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
