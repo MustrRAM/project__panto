3bef4f1715255cd78c52cb3699aafebaac1fd8b3
Mustafa Ramazanov <mustafa.ramazanov.2002@gmail.com>
2023-07-07 15:24:21 +0300

    first-finish

--- js/main.js
+++ js/main.js
@@ -1,45 +1,10 @@
-    swiper = new Swiper('.swiper', {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        freeMode: true,
-        
-    
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev' ,
-          },
-          clickable: true,
-       
-          breakpoints:{
-            320: {
-                slidesPerView:1, 
-             },
-            500: {
-               slidesPerView:2, 
-            },
-            900: {
-                slidesPerView:3, 
-             },
-             1200: {
-                slidesPerView:4, 
-             }
-          },
-
-      
-
-});
-
-
-
 const menuBtn = document.querySelector('.menu__btn');
 const menu = document.querySelector('.menu');
 
 menuBtn.addEventListener('click', function(){
     menu.classList.toggle('menu--active');
         menuBtn.classList.toggle('active')
-})
-
-
+});
 
 const menuLink = document.querySelector('.menu__link');
 const menuLinkBtn = document.querySelector('.menu__link-btn')
@@ -48,8 +13,7 @@ const menuSwipe = document.querySelector('.menu__list-swipe');
 menuLink.addEventListener('click', () => {
     menuSwipe.classList.toggle('menu__list-swipe--active');
     menuLinkBtn.classList.toggle('menu__link-btn--active')
-})
-
+});
 
 const priceBtn = document.querySelectorAll('.price__bar-list')
 
@@ -67,8 +31,6 @@ function clearActiveClasses(){
     })
 }
 
-
-
 function onEntry(entry) {
     entry.forEach(change => {
       if (change.isIntersecting) {
@@ -84,3 +46,72 @@ function onEntry(entry) {
   for (let elm of elements) {
     observer.observe(elm);
   }
+
+
+  swiper = new Swiper('.swiper', {
+    slidesPerView: 4,
+    spaceBetween: 40,
+    freeMode: true,
+    
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev' ,
+      },
+      clickable: true,
+   
+      breakpoints:{
+        320: {
+            slidesPerView:1, 
+         },
+        500: {
+           slidesPerView:2, 
+        },
+        900: {
+            slidesPerView:3, 
+         },
+         1200: {
+            slidesPerView:4, 
+         }
+      },
+
+  
+
+});
+
+
+
+$('.reviews__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite:false,
+    responsive: [
+        {
+            breakpoint: 1250,
+            settings: {
+                slidesToShow:2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 820,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+
+            }
+        },
+
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                fade:true,
+            }
+        },
+ 
+    ]
+
+  });
